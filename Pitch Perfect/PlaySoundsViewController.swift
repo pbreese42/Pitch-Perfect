c5607e9afc65fbbd58f4168de5417c70b6952f96
pbreese42 <philip@Philips-Mac.local>
2016-02-26 22:46:47 -0500

    Added elements to PlaySoundsViewController to play fast and slow audio

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = try! AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate = true
+        }
+        else {
+            print("the filePath is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +31,21 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func playSlowAudio(sender: AnyObject) {
-        
+    func playAudio(rate: Float) {
+        audioPlayer.stop()
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
+    @IBAction func playSlowAudio(sender: UIButton) {
+        playAudio(0.5)
+    }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        playAudio(1.5)
+    }
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
     }
 }
